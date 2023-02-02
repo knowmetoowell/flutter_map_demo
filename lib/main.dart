e9417f1276d4dff692a5c6900382ac83734afeae
DESKTOP-GBDJKT8\이상원 <kumct12@naver.com>
2023-02-02 16:31:57 +0900

    drawer 추가 및 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didfmnryjmryjnn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,33 +34,176 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Welcome to AppBar'),
+        centerTitle: true,
+        //leading: IconButton(icon: Icon(Icons.menu), onPressed: null),
+        // actions: [
+        //   IconButton(icon: Icon(Icons.image), onPressed: null),
+        //   IconButton(icon: Icon(Icons.navigate_next), onPressed: null),
+        // ],
+        backgroundColor: Colors.red,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero, // 여백은 필요 없다.
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("yong"),
+              accountEmail: Text("otrodevym@gmail.com"),
+              currentAccountPicture: const CircleAvatar(
+                backgroundColor: Colors.white,
+                //backgroundImage: AssetImage('assets/images/1.jpg'),
+              ),
+              otherAccountsPictures: const [
+                // currentAccountPicture와 같은 형태지만 복수로 지정 가능
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  //backgroundImage: AssetImage('assets/images/2.gif'),
+                ),
+                // CircleAvatar( // 복수가 되는지 테스트
+                //   backgroundColor: Colors.white,
+                //   backgroundImage: AssetImage('assets/images/2.gif'),
+                // )
+              ],
+              onDetailsPressed: () => {
+                // 더보기 화살표를 구현
+                print("clicked")
+              },
+              decoration: BoxDecoration(
+                  color: Colors.red[200],
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(40.0),
+                      bottomRight: Radius.circular(40.0))), // 빨간 부분을 꾸미기 위한 기능
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Home2"),
+              onTap: () => {
+
+              },
+              trailing: Icon(Icons.expand_more), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.settings, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Setting"),
+              onTap: () => {
+                print("Setting!!")
+              },
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer, color: Colors.grey[850],), // 화면의 첫 부분
+              title: Text("Q&A"),
+              onTap: () => {
+                print("Q&A!!")
+              },
+
+              trailing: Icon(Icons.add), // 화면의 끝 부분
+            ),
+          ],
+        ),
       ),
       body: Center(
 
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
